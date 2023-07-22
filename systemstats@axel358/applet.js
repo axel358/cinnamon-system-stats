207c1e6350ae96bdc569292dd75f8a7ab8e9cc20
Axel358 <edgar.serrano.9803@gmail.com>
2023-07-22 10:41:27 -0400

    feat: Additional info on tooltip

--- systemstats@axel358/applet.js
+++ systemstats@axel358/applet.js
@@ -5,7 +5,6 @@ const St = imports.gi.St;
 const GTop = imports.gi.GTop;
 const Mainloop = imports.mainloop;
 const Settings = imports.ui.settings;
-
 class SystemStatsApplet extends Applet.TextApplet {
 
     constructor(metadata, orientation, panel_height, instance_id) {
@@ -16,8 +15,12 @@ class SystemStatsApplet extends Applet.TextApplet {
         this.settings.bind("decimal-places", "decimal_places", this.on_settings_changed);
         this.settings.bind("display-style", "display_style", this.on_settings_changed);
 
+        this._applet_tooltip._tooltip.set_style("text-align:left");
+
         this.cpu = new GTop.glibtop_cpu();
         this.mem = new GTop.glibtop_mem();
+        this.disk = new GTop.glibtop_fsusage();
+        this.uptime = new GTop.glibtop_uptime();
 
         this.update();
     }
@@ -34,27 +37,47 @@ class SystemStatsApplet extends Applet.TextApplet {
 
         this.last_cpu = this.cpu.total;
         const formatted_cpu = "CPU: " + cpu_now + "% ";
+        const formatted_cpu_bold = "<b>CPU: </b>" + cpu_now + "% ";
 
         //Memory usage
         GTop.glibtop_get_mem(this.mem);
-        const mem = (this.mem.user / this.mem.total * 100);
+        const mem_used = (this.mem.user / this.mem.total * 100);
+        const formatted_mem_info = "<b>RAM: </b>" + this.formatBytes(this.mem.user) + " / " + this.formatBytes(this.mem.total);
+        const formatted_mem_used = "RAM: " + mem_used.toFixed(this.decimal_places) + "% ";
 
-        const formatted_mem = "RAM: " + mem.toFixed(this.decimal_places) + "% ";
+        //Disk
+        GTop.glibtop_get_fsusage(this.disk, "/");
+        const disk_used_root = (this.disk.blocks - this.disk.bfree) / this.disk.blocks * 100;
+        const formatted_disk_used_root = "<b>Used disk(/): </b>" + disk_used_root.toFixed(this.decimal_places) + "%";
 
-        this.set_applet_tooltip("");
+        GTop.glibtop_get_fsusage(this.disk, "/home");
+        const disk_used_home = (this.disk.blocks - this.disk.bfree) / this.disk.blocks * 100;
+        const formatted_disk_used_home = "<b>Used disk(/home): </b>" + disk_used_home.toFixed(this.decimal_places) + "%";
+
+
+        //Up time
+        GTop.glibtop_get_uptime(this.uptime);
+        const formatted_uptime = "<b>Up time: </b>" + this.uptime.uptime + "s";
+
+
+        this.set_applet_tooltip(formatted_mem_info
+            + "\n" + formatted_cpu_bold
+            + "\n" + formatted_disk_used_root
+            + "\n" + formatted_disk_used_home
+            + "\n" + formatted_uptime, true);
 
         switch(this.display_style){
             case "column":
-                this.set_applet_label(formatted_cpu + "\n" + formatted_mem);
+                this.set_applet_label(formatted_cpu + "\n" + formatted_mem_used);
                 break;
             case "both":
-                this.set_applet_label(formatted_cpu + " " + formatted_mem);
+                this.set_applet_label(formatted_cpu + " " + formatted_mem_used);
                 break;
             case "cpu":
                 this.set_applet_label(formatted_cpu);
                break;
             case "mem":
-                this.set_applet_label(formatted_mem);
+                this.set_applet_label(formatted_mem_used);
         }
 
         this.update_loop_id = Mainloop.timeout_add(this.refresh_interval, Lang.bind(this, this.update));
