2c61ec446e9ab84e9df33b460b4d7aceefcd3e22
Axel358 <edgar.serrano.9803@gmail.com>
2023-07-25 10:10:34 -0400

    fix: Use a popup menu instead of tooltip for additional info

--- systemstats@axel358/applet.js
+++ systemstats@axel358/applet.js
@@ -5,7 +5,8 @@ const St = imports.gi.St;
 const GTop = imports.gi.GTop;
 const Mainloop = imports.mainloop;
 const Settings = imports.ui.settings;
-const GLib = imports.gi.GLib;
+const Util = imports.misc.util;
+const PopupMenu = imports.ui.popupMenu;
 
 class SystemStatsApplet extends Applet.TextApplet {
 
@@ -18,7 +19,19 @@ class SystemStatsApplet extends Applet.TextApplet {
         this.settings.bind("display-style", "display_style", this.on_settings_changed);
         this.settings.bind("use-compact-label", "use_compact_label", this.on_settings_changed);
 
-        this._applet_tooltip._tooltip.set_style("text-align:left");
+        this.set_applet_tooltip("Click for more details");
+
+        const menu_manager = new PopupMenu.PopupMenuManager(this);
+        this.menu = new Applet.AppletPopupMenu(this, orientation);
+        menu_manager.addMenu(this.menu);
+
+        this.info_menu_item = new PopupMenu.PopupMenuItem("", { reactive: false });
+        this.menu.addMenuItem(this.info_menu_item);
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        const item = new PopupMenu.PopupMenuItem(_("Open System Monitor"));
+        item.connect('activate', () => Util.spawnCommandLine("gnome-system-monitor"));
+        this.menu.addMenuItem(item);
 
         this.cpu = new GTop.glibtop_cpu();
         this.mem = new GTop.glibtop_mem();
@@ -33,6 +46,10 @@ class SystemStatsApplet extends Applet.TextApplet {
         //this.update();
     }
 
+    on_applet_clicked(event) {
+        this.menu.toggle();
+    }
+
     update() {
         //CPU usage
         GTop.glibtop_get_cpu(this.cpu);
@@ -40,36 +57,14 @@ class SystemStatsApplet extends Applet.TextApplet {
         const cpu_used_now = this.cpu.total - this.cpu.idle - this.cpu_last_used;
         const cpu_usage = cpu_used_now / cpu_total_now * 100;
         const formatted_cpu = (this.use_compact_label ? "C: " : "CPU: ") + cpu_usage.toFixed(this.decimal_places) + "% ";
-        const formatted_cpu_bold = "<b>CPU: </b>" + cpu_usage.toFixed(this.decimal_places) + "% ";
+
         this.cpu_last_total = this.cpu.total;
         this.cpu_last_used = this.cpu.total - this.cpu.idle;
 
         //Memory usage
         GTop.glibtop_get_mem(this.mem);
         const mem_used = this.mem.user / this.mem.total * 100;
-        const formatted_mem_info = "<b>RAM: </b>" + this.formatBytes(this.mem.user) + " / " + this.formatBytes(this.mem.total);
         const formatted_mem_used = (this.use_compact_label ? "R: " : "RAM: ") + mem_used.toFixed(this.decimal_places) + "% ";
-
-        //Disk
-        GTop.glibtop_get_fsusage(this.disk, "/");
-        const disk_used_root = (this.disk.blocks - this.disk.bfree) / this.disk.blocks * 100;
-        const formatted_disk_used_root = "<b>Used disk(/): </b>" + disk_used_root.toFixed(this.decimal_places) + "%";
-
-        GTop.glibtop_get_fsusage(this.disk, "/home");
-        const disk_used_home = (this.disk.blocks - this.disk.bfree) / this.disk.blocks * 100;
-        const formatted_disk_used_home = "<b>Used disk(/home): </b>" + disk_used_home.toFixed(this.decimal_places) + "%";
-
-
-        //Up time
-        GTop.glibtop_get_uptime(this.uptime);
-        const formatted_uptime = "<b>Up time: </b>" + this.uptime.uptime + "s";
-
-
-        this.set_applet_tooltip(formatted_mem_info
-            + "\n" + formatted_cpu_bold
-            + "\n" + formatted_disk_used_root
-            + "\n" + formatted_disk_used_home
-            + "\n" + formatted_uptime, true);
 
         switch (this.display_style) {
             case "column":
@@ -83,6 +78,33 @@ class SystemStatsApplet extends Applet.TextApplet {
                 break;
             case "mem":
                 this.set_applet_label(formatted_mem_used);
+        }
+
+        //Only retrieve additional info if the menu is open
+        if (this.menu.isOpen) {
+            const formatted_cpu_bold = "<b>CPU: </b>" + cpu_usage.toFixed(this.decimal_places) + "% ";
+            const formatted_mem_info = "<b>RAM: </b>" + this.formatBytes(this.mem.user) + " / " + this.formatBytes(this.mem.total);
+
+            //Disk
+            GTop.glibtop_get_fsusage(this.disk, "/");
+            const disk_used_root = (this.disk.blocks - this.disk.bfree) / this.disk.blocks * 100;
+            const formatted_disk_used_root = "<b>Used disk(/): </b>" + disk_used_root.toFixed(this.decimal_places) + "%";
+
+            GTop.glibtop_get_fsusage(this.disk, "/home");
+            const disk_used_home = (this.disk.blocks - this.disk.bfree) / this.disk.blocks * 100;
+            const formatted_disk_used_home = "<b>Used disk(/home): </b>" + disk_used_home.toFixed(this.decimal_places) + "%";
+
+
+            //Up time
+            GTop.glibtop_get_uptime(this.uptime);
+            const formatted_uptime = "<b>Up time: </b>" + this.uptime.uptime + "s";
+
+
+            this.info_menu_item.label.get_clutter_text().set_markup(formatted_mem_info
+                + "\n" + formatted_cpu_bold
+                + "\n" + formatted_disk_used_root
+                + "\n" + formatted_disk_used_home
+                + "\n" + formatted_uptime, true);
         }
 
         this.update_loop_id = Mainloop.timeout_add(this.refresh_interval, Lang.bind(this, this.update));
